4371788e88c730c38774913f185ba05a9431541b
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-20 16:34:19 +0100

    Update AdminPanel.js

--- frontend/src/components/AdminPanel.js
+++ frontend/src/components/AdminPanel.js
@@ -1,1 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ActualizarApp from './ActualizarApp';
+
+const AdminPanel = () => {
+  return (
+    <div>
+      <h1>Panel de Administración</h1>
+      <div className="admin-panel">
+        <div className="admin-panel-section">
+          <h2>Gestión de Formularios</h2>
+          <Link to="/admin/formularios">Ver Formularios</Link>
+          <Link to="/admin/crear_formulario">Crear Nuevo Formulario</Link>
+        </div>
+
+        <div className="admin-panel-section">
+          <h2>Gestión de Usuarios</h2>
+          <Link to="/admin/usuarios">Ver Usuarios</Link>
+          <Link to="/admin/subir_csv">Subir Usuarios desde CSV</Link>
+        </div>
+
+        <div className="admin-panel-section">
+          <h2>Actualizar Aplicación</h2>
+          <ActualizarApp />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminPanel;
 
