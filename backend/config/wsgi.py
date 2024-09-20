b42d89dd551d20c3176e7c6a6ff90e125f24abf6
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-20 16:22:25 +0100

    Update wsgi.py

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,1 +1,18 @@
+"""
+WSGI config for el proyecto de Red de Innovación de FP de Canarias.
+
+Este archivo expone la aplicación WSGI como una variable llamada `application`.
+
+Para más información sobre este archivo, consulta:
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Establecer la configuración predeterminada para el entorno de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Definir la aplicación WSGI
+application = get_wsgi_application()
 
