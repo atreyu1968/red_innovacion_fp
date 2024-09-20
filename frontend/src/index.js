4dc64d7397ad43bee75ded0781f28870af7185d2
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-20 16:49:50 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Importar estilos globales
+import App from './App';  // Importar el componente App principal
+import { BrowserRouter } from 'react-router-dom';  // Importar el enrutador
+
+// Renderizar la aplicación dentro de un Router
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')  // Montar la aplicación en el elemento con id 'root'
+);
 
