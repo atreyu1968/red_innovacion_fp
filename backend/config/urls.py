db8bf0ad303d4784e5f0e937355cfa15a4ecb0da
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-20 16:20:43 +0100

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,1 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Ruta para el panel de administración
+    path('admin/', admin.site.urls),
+
+    # Rutas para la aplicación de usuarios
+    path('api/auth/', include('usuarios.urls')),
+
+    # Rutas para la aplicación de formularios
+    path('api/formularios/', include('formularios.urls')),
+]
+
+# Sirviendo archivos estáticos y media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
