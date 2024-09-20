40315c79c297fce0f2b83642c31c3fd3258994bf
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-09-20 15:29:05 +0100

    Update permissions.py

--- backend/usuarios /permissions.py
+++ backend/usuarios /permissions.py
@@ -1,1 +1,23 @@
 
+from rest_framework.permissions import BasePermission
+
+class IsAdmin(BasePermission):
+    """
+    Permite el acceso solo a los administradores.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'ADMINISTRADOR'
+
+class IsCoordinador(BasePermission):
+    """
+    Permite el acceso solo a los coordinadores de red.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'COORDINADOR'
+
+class IsGestor(BasePermission):
+    """
+    Permite el acceso solo a los gestores de red.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'GESTOR'
